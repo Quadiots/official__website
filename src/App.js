8c9924aaacc60a6485f4b3ffc974a962ea5b5311
Shubham Dalvi <shubham.dalvi008@gmail.com>
2021-04-28 16:03:10 +0530

    Added new animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
+import React from "react";
 import logo from "./images/Quadiots__logo.jpeg";
-import Tilt from "react-vanilla-tilt";
+import Tilt from "react-parallax-tilt";
 
 function App() {
   return (
-    <div className="app">
-      <Tilt className="product">
-        <img src={logo} alt="" />
-      </Tilt>
-      <a className="twitter__style" href="https://twitter.com/quadiots">
-        {" "}
-        Follow Us on Twitter
-      </a>
+    <div>
+      <div className="app">
+        <Tilt scale={1.15} className="product">
+          <img src={logo} alt="" />
+        </Tilt>
+        <a className="twitter__style" href="https://twitter.com/quadiots">
+          {" "}
+          Follow Us on Twitter
+        </a>
+      </div>
     </div>
   );
 }
